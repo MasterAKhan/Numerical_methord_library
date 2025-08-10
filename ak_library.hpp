1bfcfba70aab7bbd955eb1d742d17439d1ddb625
Ahshan Khan <73234686+MasterAKhan@users.noreply.github.com>
2025-08-10 19:14:45 +0530

    first commit

---
+++ ak_library.hpp
@@ -0,0 +1,13 @@
+#ifndef AK_LIBRARY_HPP
+#define AK_LIBRARY_HPP
+
+// ak_library.hpp
+// Numerical Method Library - Header File
+
+namespace ak {
+
+    // Add your function declarations and classes here
+
+}
+
+#endif // AK_LIBRARY_HPP
